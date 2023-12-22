945376c05b1c5e3c364e6727b29d018124fda0ce
Arnold Szederjesi <szederjesiarnold@gmail.com>
2023-12-22 21:51:09 +0200

    Solve Day 12B

--- Sources/AdventOfCode2023/Day12.swift
+++ Sources/AdventOfCode2023/Day12.swift
@@ -18,7 +18,7 @@ class Day12: Day {
 
     func b() async throws {
         let lines = try await Common.readLines(filePath: filePath)
-        let result = f(lines)
+        let result = parseConditionRecordsAndExpandAndCalculateSumOfPossibleArrangements(lines)
         print("B: \(result)")
     }
 
@@ -34,6 +34,69 @@ class Day12: Day {
             .reduce(0, +)
     }
 
+    func parseConditionRecordsAndExpandAndCalculateSumOfPossibleArrangements(_ lines: [String]) -> Int {
+        let conditionRecords = parseConditionRecords(lines)
+        return conditionRecords.map { cr in
+                let newSprings = (1...5).map { _ in
+                        cr.springs
+                    }
+                    .joined(separator: "?")
+                let newCheckSum = (1...5).flatMap { _ in
+                    cr.checkSum
+                }
+                let newConditionRecord = (newSprings, newCheckSum)
+                return quickCountPossibleArrangements(newConditionRecord)
+            }
+            .reduce(0, +)
+    }
+
+    func quickCountPossibleArrangements(_ conditionRecord: ConditionRecord) -> Int {
+        struct Key: Hashable {
+            let checkSumIndex: Int
+            let consecutiveBrokenSpringsCount: Int
+            let nextIsDot: Bool
+        }
+
+        typealias Arrangements = [Key: Int]
+        var arrangements = [Key(checkSumIndex: 0, consecutiveBrokenSpringsCount: 0, nextIsDot: false): 1]
+
+        for spring in conditionRecord.springs {
+            var newArrangements = Arrangements()
+            for (key, count) in arrangements {
+                var checkSumIndex = key.checkSumIndex
+                var consecutiveBrokenSpringsCount = key.consecutiveBrokenSpringsCount
+                var nextIsDot = key.nextIsDot
+                if (spring == "#" || spring == "?") && checkSumIndex < conditionRecord.checkSum.count && !nextIsDot {
+                    if spring == "?" && consecutiveBrokenSpringsCount == 0 {
+                        newArrangements[key, default: 0] += count
+                    }
+                    consecutiveBrokenSpringsCount += 1
+                    if consecutiveBrokenSpringsCount == conditionRecord.checkSum[checkSumIndex] {
+                        checkSumIndex = checkSumIndex + 1
+                        consecutiveBrokenSpringsCount = 0
+                        nextIsDot = true
+                    }
+                    let newKey = Key(checkSumIndex: checkSumIndex, consecutiveBrokenSpringsCount: consecutiveBrokenSpringsCount, nextIsDot: nextIsDot)
+                    newArrangements[newKey, default: 0] += count
+                } else if (spring == "." || spring == "?") && consecutiveBrokenSpringsCount == 0 {
+                    let newKey = Key(checkSumIndex: checkSumIndex, consecutiveBrokenSpringsCount: consecutiveBrokenSpringsCount, nextIsDot: false)
+                    newArrangements[newKey, default: 0] += count
+                }
+            }
+            arrangements = newArrangements
+        }
+
+        return
+            arrangements
+                .filter {
+                    $0.key.checkSumIndex == conditionRecord.checkSum.count
+                }
+                .map {
+                    $0.value
+                }
+                .reduce(0, +)
+    }
+
     func countOfPossibleArrangements(_ conditionRecord: ConditionRecord) -> Int {
         let (springs, checkSum) = conditionRecord
         let possibleArrangements = generatePossibleArrangements(springs, checkSum: checkSum)
@@ -46,12 +109,18 @@ class Day12: Day {
             switch spring {
             case ".", "#":
                 partialArrangements = partialArrangements.map {
-                    $0 + String(spring)
-                }.filter{ validPartialArrangement($0, checkSum: checkSum) }
+                        $0 + String(spring)
+                    }
+                    .filter {
+                        validPartialArrangement($0, checkSum: checkSum)
+                    }
             case "?":
                 partialArrangements = partialArrangements.flatMap({
-                    [$0 + ".", $0 + "#"]
-                }).filter{ validPartialArrangement($0, checkSum: checkSum) }
+                        [$0 + ".", $0 + "#"]
+                    })
+                    .filter {
+                        validPartialArrangement($0, checkSum: checkSum)
+                    }
             default:
                 fatalError("Unknown spring: \(spring)")
             }
@@ -150,5 +219,7 @@ class Day12: Day {
         assert(countOfPossibleArrangements(("???.###", [1, 1, 3])) == 1)
         assert(countOfPossibleArrangements(("????.######..#####.", [1, 6, 5])) == 4)
         assert(countOfPossibleArrangements(("?###????????", [3, 2, 1])) == 10)
+
+        assert(parseConditionRecordsAndExpandAndCalculateSumOfPossibleArrangements(example1) == 525152)
     }
 }
